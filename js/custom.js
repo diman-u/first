a73d15195fc9c7486832a6db224a72b351104a19
Dmitry U <uda78@yandex.ru>
2017-03-24 08:08:54 +0300

    Добавил img_hovers и сделал кнопку телефона

--- js/custom.js
+++ js/custom.js
@@ -20,38 +20,14 @@ $(document).ready(function() {
         $(this).next('ul').slideToggle('300');
     });
 
-    var sv = $('.svg').css('background');
-    console.log(sv);
-
-
-    jQuery('a.svg').each(function(){
-            var $img = jQuery(this);
-            var imgID = $img.attr('id');
-            var imgClass = $img.attr('class');
-            var imgURL = $img.attr('src');
+    
+    $(".right_mobile").on("click", function () {
+        if ( $('.right').css('display')=='none') {
+                $('.right').css('display', 'block');
+                $('.right').addClass('mobile');                
             
-            console.log($img);
-
-            jQuery.get(imgURL, function(data) {
-                // Get the SVG tag, ignore the rest
-                var $svg = jQuery(data).find('svg');
-
-                // Add replaced image's ID to the new SVG
-                if(typeof imgID !== 'undefined') {
-                    $svg = $svg.attr('id', imgID);
-                }
-                // Add replaced image's classes to the new SVG
-                if(typeof imgClass !== 'undefined') {
-                    $svg = $svg.attr('class', imgClass+' replaced-svg');
-                }
-
-                // Remove any invalid XML tags as per http://validator.w3.org
-                $svg = $svg.removeAttr('xmlns:a');
-
-                // Replace image with new SVG
-                $img.replaceWith($svg);
-
-            }, 'xml');
-
-        });
+        } else{
+                $('.right').css('display', 'none');                
+        }
+    });
 });
