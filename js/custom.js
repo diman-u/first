738a811ee839a6788417516b6e4fe3cd5930e77e
Alexey Lavinsky <cap-Bernardito@yandex.ru>
2017-03-27 12:59:34 +0300

    pravki

--- js/custom.js
+++ js/custom.js
@@ -1,24 +1,24 @@
 $(document).ready(function() {
     $("a.menu").on('click', function () {
-
-        $(this).toggleClass('active');
-        $('.open-menu').slideToggle('300');
         
-        setTimeout(function(){
-
-            
+                    $(this).toggleClass('active');
+           
             if ( $('#content').css('display')=='none') {
-            		$('#content').css('display', 'block');
-                    $('#footer').css('display', 'flex');
-            		$('#sochide').css('display', 'flex');
-                    
+                $('#content').css('display', 'block');
+                $('#footer').css('display', 'flex');
+                $('#sochide').css('display', 'flex');
+                setTimeout(function(){
+                    $('.open-menu').slideToggle('300');
+                }, 0);
             } else{
-            	$('#content').css('display', 'none');
-                $('#footer').css('display', 'none');
-            	$('#sochide').css('display', 'none');
+                $('.open-menu').slideToggle('300');
+                setTimeout(function(){
+                    	$('#content').css('display', 'none');
+                        $('#footer').css('display', 'none');
+                    	$('#sochide').css('display', 'none');
+                }, 500);
             }
 
-        }, 500);
         
     });   
     $(".menu-items.wells>a").on("click", function () {
