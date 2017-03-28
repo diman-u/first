34cfaceb4d1584440c30e2fe9a1c310fef28afa2
Dmitry U <uda78@yandex.ru>
2017-03-28 15:37:28 +0300

    Правки 28.03

---
+++ specialist_filter.php
@@ -0,0 +1,231 @@
+<?php include "head.php" ?>
+<?php include "header.php" ?>
+
+    <main  id="content">
+    
+
+        <section class="contentbox-doctors">
+
+            <h1 class="contentbox-doctors__main-head">Специалисты</h1>
+
+            <h4 class="contentbox-doctors__head">Флоат студия – команда высококласнных специалистов в своих областях. Здесь всегда атмосфера доверия и заботы.</h4>
+
+            <div class="contentbox-doctors__buttons">
+                <div class="contentbox-doctors__buttons-label">Показать:</div>
+                <a class="more" href="#">Все специалисты</a>
+                <a class="more more-active" href="#">По направлению</a>
+            </div>
+
+            <div class="sort-doctors">
+                <ul class="sort-doctors__list">
+                    <li class="sort-doctors__item"><a href="#">Релаксация и антистресс</a></li>
+                    <li class="sort-doctors__item sort-doctors__item-active"><a href="#">Здоровая беременность</a></li>
+                    <li class="sort-doctors__item"><a href="#">Натуральная красота и омоложение</a></li>
+                    <li class="sort-doctors__item"><a href="#">Восстановление спортсменов</a></li>
+                    <li class="sort-doctors__item"><a href="#">Оздоровление питанием</a></li>
+                    <li class="sort-doctors__item"><a href="#">Лайф- и Wellness-коучинг</a></li>
+                </ul>
+            </div>
+
+            <h4 class="contentbox-doctors__head-row">Здоровая беременность</h4>
+
+            <div class="doctors-grid">
+                <div class="doctors-grid__item">
+                    <div class="doctors-grid__photo">
+                        <img src="assets/newdis/img/foto-1.jpg">
+                    </div>
+                    <div class="doctors-grid__buttons">
+                        <a class="more more-invert" href="#">Подробнее</a>
+                    </div>
+                    <h4 class="doctors-grid__head">Котенев Владимир</h4>
+                    <div class="doctors-grid__skills">
+                        <ul>
+                            <li>Авторский Ца Ха Ки</li>
+                            <li>Классический массаж</li>
+                            <li>Тибетский массаж</li>
+                            <li>Массаж камнями</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="doctors-grid__item">
+                    <div class="doctors-grid__photo">
+                        <img src="assets/newdis/img/foto-2.jpg">
+                    </div>
+                    <div class="doctors-grid__buttons">
+                        <a class="more more-invert" href="#">Подробнее</a>
+                    </div>
+                    <h4 class="doctors-grid__head">Казарновская Ольга</h4>
+                    <div class="doctors-grid__skills">
+                        <ul>
+                            <li>Авторский Ца Ха Ки</li>
+                            <li>Классический массаж</li>
+                            <li>Тибетский массаж</li>
+                            <li>Массаж камнями</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="doctors-grid__item">
+                    <div class="doctors-grid__photo">
+                        <img src="assets/newdis/img/foto-3.jpg">
+                    </div>
+                    <div class="doctors-grid__buttons">
+                        <a class="more more-invert" href="#">Подробнее</a>
+                    </div>
+                    <h4 class="doctors-grid__head">Товбина Юлия</h4>
+                    <div class="doctors-grid__skills">
+                        <ul>
+                            <li>Авторский Ца Ха Ки</li>
+                            <li>Классический массаж</li>
+                            <li>Тибетский массаж</li>
+                            <li>Массаж камнями</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="doctors-grid__item">
+                    <div class="doctors-grid__photo">
+                        <img src="assets/newdis/img/foto-4.jpg">
+                    </div>
+                    <div class="doctors-grid__buttons">
+                        <a class="more more-invert" href="#">Подробнее</a>
+                    </div>
+                    <h4 class="doctors-grid__head">Жохова Мария</h4>
+                    <div class="doctors-grid__skills">
+                        <ul>
+                            <li>Авторский Ца Ха Ки</li>
+                            <li>Классический массаж</li>
+                            <li>Тибетский массаж</li>
+                            <li>Массаж камнями</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+
+            <h4 class="contentbox-doctors__head-row contentbox-doctors__head-row-upjump">ОСТЕОПАТИЯ И ИНТЕГРАЛЬНЫЕ МЕТОДЫ ВОССТАНОВЛЕНИЯ</h4>
+
+            <div class="doctors-grid">
+                <div class="doctors-grid__item">
+                    <div class="doctors-grid__photo">
+                        <img src="assets/newdis/img/foto-1.jpg">
+                    </div>
+                    <div class="doctors-grid__buttons">
+                        <a class="more more-invert" href="#">Подробнее</a>
+                    </div>
+                    <h4 class="doctors-grid__head">Котенев Владимир</h4>
+                    <div class="doctors-grid__skills">
+                        <ul>
+                            <li>Авторский Ца Ха Ки</li>
+                            <li>Классический массаж</li>
+                            <li>Тибетский массаж</li>
+                            <li>Массаж камнями</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="doctors-grid__item">
+                    <div class="doctors-grid__photo">
+                        <img src="assets/newdis/img/foto-2.jpg">
+                    </div>
+                    <div class="doctors-grid__buttons">
+                        <a class="more more-invert" href="#">Подробнее</a>
+                    </div>
+                    <h4 class="doctors-grid__head">Казарновская Ольга</h4>
+                    <div class="doctors-grid__skills">
+                        <ul>
+                            <li>Авторский Ца Ха Ки</li>
+                            <li>Классический массаж</li>
+                            <li>Тибетский массаж</li>
+                            <li>Массаж камнями</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="doctors-grid__item">
+                    <div class="doctors-grid__photo">
+                        <img src="assets/newdis/img/foto-3.jpg">
+                    </div>
+                    <div class="doctors-grid__buttons">
+                        <a class="more more-invert" href="#">Подробнее</a>
+                    </div>
+                    <h4 class="doctors-grid__head">Товбина Юлия</h4>
+                    <div class="doctors-grid__skills">
+                        <ul>
+                            <li>Авторский Ца Ха Ки</li>
+                            <li>Классический массаж</li>
+                            <li>Тибетский массаж</li>
+                            <li>Массаж камнями</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="doctors-grid__item">
+                    <div class="doctors-grid__photo">
+                        <img src="assets/newdis/img/foto-4.jpg">
+                    </div>
+                    <div class="doctors-grid__buttons">
+                        <a class="more more-invert" href="#">Подробнее</a>
+                    </div>
+                    <h4 class="doctors-grid__head">Жохова Мария</h4>
+                    <div class="doctors-grid__skills">
+                        <ul>
+                            <li>Авторский Ца Ха Ки</li>
+                            <li>Классический массаж</li>
+                            <li>Тибетский массаж</li>
+                            <li>Массаж камнями</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <h4 class="contentbox-doctors__head-row contentbox-doctors__head-row-upjump">УХОДЫ ЗА ЛИЦОМ И ТЕЛОМ</h4>
+
+            <div class="doctors-grid">
+                <div class="doctors-grid__item">
+                    <div class="doctors-grid__photo">
+                        <img src="assets/newdis/img/foto-1.jpg">
+                    </div>
+                    <div class="doctors-grid__buttons">
+                        <a class="more more-invert" href="#">Подробнее</a>
+                    </div>
+                    <h4 class="doctors-grid__head">Котенев Владимир</h4>
+                    <div class="doctors-grid__skills">
+                        <ul>
+                            <li>Авторский Ца Ха Ки</li>
+                            <li>Классический массаж</li>
+                            <li>Тибетский массаж</li>
+                            <li>Массаж камнями</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="doctors-grid__item">
+                    <div class="doctors-grid__photo">
+                        <img src="assets/newdis/img/foto-2.jpg">
+                    </div>
+                    <div class="doctors-grid__buttons">
+                        <a class="more more-invert" href="#">Подробнее</a>
+                    </div>
+                    <h4 class="doctors-grid__head">Казарновская Ольга</h4>
+                    <div class="doctors-grid__skills">
+                        <ul>
+                            <li>Авторский Ца Ха Ки</li>
+                            <li>Классический массаж</li>
+                            <li>Тибетский массаж</li>
+                            <li>Массаж камнями</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+
+
+        <section class="contentbox contentbox-not-margin">
+
+            <h4 class="contentbox__phones"><a href="tel:84991105210">+7 (499) 110-52-10</a></h4>
+
+        </section>
+
+
+    </main>
+
+<?php include "footer.php" ?>
+
+</body>
+</html>
