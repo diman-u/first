5969918a0fa5e245bae52308a62fbf9eb2d1ae83
Dmitry U <uda78@yandex.ru>
2017-03-28 15:09:47 +0300

    floating

---
+++ floating.php
@@ -0,0 +1,272 @@
+<?php include "head.php" ?>
+<?php include "header.php" ?>
+
+<main  id="content">
+
+
+    <section class="photobox">
+
+        <img class="photobox__img" src="assets/newdis/img/relax-floating.jpg">
+        <div class="photobox__wrap">
+            <div class="photobox__content photobox__content-around">
+                <img src="assets/newdis/img/scrolling_light.svg" alt="" class="photobox__scroll">
+                <div>
+                    <h4 class="photobox__head">Релаксация и востановление</h4>
+                    <div class="photobox__content-crop-with">
+                        <p>Единственная в России флоатинг-студия открытого типа. Уникальные программы глубинного восстановления тела и разума.</p>
+                    </div>
+                </div>
+            <div class="photobox__button photobox__button-check"><a class="more" href="">Что такое флоатинг?</a>
+            </div>
+            </div>
+        </div>
+
+    </section>
+
+
+    <section class="contentbox">
+
+        <h4 class="contentbox__head">Что такое флоатинг?</h4>
+        <div class="contentbox__promo">Самый эффективный и быстрый метод глубочайшего расслабления и восстановления за 60 минут, недостижимый другими способами. Позволяет быстро и своевременно останавливать стресс и беспокойство человека. </div>
+
+    </section>
+
+
+    <section class="contentbox contentbox-bg waves-top">
+
+        <h4 class="contentbox-bg__head">Флоатинг – релаксация и восстановление в невесомости</h4>
+
+        <div class="contentbox-bg__promo">Флоатинг «обновляет» тело и разум, стирает негативные эмоции и программы, делая человека менее восприимчивым к стрессам. Сеансы флоатинга стимулируют естественную выработку гормонов счастья — эндорфинов, которые приносят ощущение радости, умиротворения и блаженства.</div>
+
+    
+        <div class="promo-grid promo-grid__compact">
+            <div class="promo-grid__item promo-grid__item-background">
+                <img src="assets/newdis/img/promo-1.png">
+                <h4 class="promo-grid__head">Расслабляет</h4>
+                <p>За счет глубого расслабления происходит максимальная разгрузка опорно-двигательного аппарата и нервной системы человека.</p>
+            </div>
+            <div class="promo-grid__item promo-grid__item-background">
+                <img src="assets/newdis/img/promo-12.png">
+                <h4 class="promo-grid__head">Делает ум ясным</h4>
+                <p>За счет синхрониttзации левого и правого полушарий формируется спокойное, сосредоточенное и целеустремленное состояние сознания, открывая доступ к неординарным методам решения проблем и задач.</p>
+            </div>
+            <div class="promo-grid__item promo-grid__item-background">
+                <img src="assets/newdis/img/promo-13.png">
+                <h4 class="promo-grid__head">Перезагружает</h4>
+                <p>Создается ощущение легкости и полного покоя, освобождая от тревог и восстанавливая внутреннюю гармонию. Человек абстрагируется от негативного влияния стрессов и эмоциональной нагрузки.</p>
+            </div>
+        </div>
+
+    </section>
+
+
+    <section class="contentbox contentbox__bluewaves">
+        <div class="contentbox__promo">1 час флоатинга дает больший успокаивающий и тонизирующий эффект, чем 6-8 часов полноценного сна. Эффект после процедуры сравним с отдыхом на море. </div>
+        <div class="contentbox__buttons">
+            <a class="more" href="#">Записаться</a>
+            <a class="more" href="#">Купить сертификат</a>
+        </div>
+    </section>
+
+
+    <section class="contentbox">
+        
+        <h4 class="contentbox__head">Как это работает?</h4>
+        <div class="contentbox__promo contentbox__promo-double-mb">Единственная в России и Восточной Европе установка флоатинга с двумя флоат-комнатами, а не капсулами.</div>
+
+        <div class="promo-grid promo-grid__compact">
+            <div class="promo-grid__item">
+                <img src="assets/newdis/img/promo-14.png">
+                <h4 class="promo-grid__head">Невесомость</h4>
+                <p class="promo-grid__text-before-button">Плотность раствора воды и английской соли позволяет телу (как на Мертвом море) «парить» на поверхности. Вместе с режимом сенсорной депривации формируется состояние, воспринимаемое как невесомость. </p>
+                <div class="contentbox__buttons">
+                    <a class="more" href="#">Подробнее</a>
+                </div>
+            </div>
+            <div class="promo-grid__item">
+                <img src="assets/newdis/img/promo-11.png">
+                <h4 class="promo-grid__head">Английская соль (MGSO4 )</h4>
+                <p class="promo-grid__text-before-button">Магний успокаивает центральную и периферическую нервную систему. Незаменим при мышечных спазмах и судорогах, проблемах с сердцем, бессоннице, напряжении в теле, нервозности, артритах, мигренях и т.д. </p>
+                <div class="contentbox__buttons">
+                    <a class="more" href="#">Подробнее</a>
+                </div>
+            </div>
+            <div class="promo-grid__item">
+                <img src="assets/newdis/img/promo-15.png">
+                <h4 class="promo-grid__head">Сенсорная депривация</h4>
+                <p class="promo-grid__text-before-button">Температура в бассейне составляет около 35 градусов, что вместе с режимом тишины и темноты позволяет полностью отключить внешние раздражители. Эффект сенсорной депривации вызывает мягкое, приятное, контролируемое ивременное изменение самосознания. </p>
+                <div class="contentbox__buttons">
+                    <a class="more" href="#">Подробнее</a>
+                </div>
+            </div>
+        </div>
+
+        <img src="assets/newdis/img/mugik-na-volnah.jpg" alt="" class="center">
+    
+    </section>
+
+
+    <section class="photobox">
+
+        <img class="photobox__img" src="assets/newdis/img/netstress-floating.jpg">
+        <div class="photobox__wrap">
+            <div class="photobox__content">
+                <h4 class="photobox__head">Скажите стрессу «нет»</h4>
+                <p>Программа «Полное погружение. Флоатинг + массаж» – это сочетание флоатинга и уникальных расслабляющих и лечебных массажей, использующих техники восточных оздоровительных систем в сочетании с классическими наработками медицинских массажей и современной телесно-ориентированной терапии. </p>
+            </div>
+        </div>
+
+    </section>
+
+
+    <section class="contentbox">
+
+        <h4 class="contentbox__head">Cвобода от стресса</h4>
+        <div class="contentbox__promo">Флоатинг + массаж в нашем центре восстановит работоспособность всегоа за 1 час. </div>
+        
+        <img src="assets/newdis/img/tony-stark.jpg" alt="" class="center mb">
+
+        <div class="contentbox__promo">Мы рекомендуем Флоат-терапию - один из методов коррекции психоэмоционального стресса и физических недомоганий с исследованием и учетом индивидуальных особенностей конкретной личности. Флоат-терапия оказывает мощный эффект сразу на всех уровнях: физическом, эмоциональном, ментальном и духовном. </div>
+
+        <div class="person">
+            <div class="person__photo">
+                <img src="assets/newdis/img/maria-gohova.jpg">
+            </div>
+            <div class="person__name">Мария Жохова</div>
+            <div class="person__status">Флоат-терапевт</div>
+        </div>
+
+        <div class="contentbox__buttons">
+            <a class="more" href="#">Выбрать программу</a>
+        </div>
+
+    </section>
+
+
+    <section class="photobox">
+
+        <img class="photobox__img" src="assets/newdis/img/video-floating.jpg">
+        <div class="photobox__wrap">
+            <div class="photobox__content">
+                <p class="photobox__videohead">Видео-ролик о программе «Полное погружение»</p>
+                <a href="#"><img src="assets/newdis/img/play.png" alt=""></a>
+            </div>
+        </div>
+
+    </section>
+
+
+    <section class="contentbox contentbox-not-margin">
+
+        <h4 class="contentbox__head">Программы релаксации и восстановления</h4>
+
+        <div class="promo-grid">
+            <div class="promo-grid__item">
+                <img src="assets/newdis/img/promo-10.png">
+                <p class="promo-grid__title">Для одного</p>
+                <p>Программы на посещение одним</p>
+            </div>
+        </div>
+
+        <div class="promo-grid-square">
+            <div class="promo-grid-square__item">
+                <img src="assets/newdis/img/certificate-1.jpg">
+                <h4 class="promo-grid-square__head"><a href="#">«Погружение» 60 минут</a></h4>
+                <p class="promo-grid-square__description">Флоатинг (60 мин.)</p>
+                <p class="promo-grid-square__price">3500 руб.</p>
+                <div class="contentbox__buttons">
+                    <a class="more" href="#">Купить</a>
+                </div>
+            </div>
+            <div class="promo-grid-square__item">
+                <img src="assets/newdis/img/certificate-2.jpg">
+                <h4 class="promo-grid-square__head"><a href="#">«Полное погружение» 120 минут</a></h4>
+                <p class="promo-grid-square__description">Флоатинг (60 мин) + Массаж (60 мин)</p>
+                <p class="promo-grid-square__price">5500 руб.</p>
+                <div class="contentbox__buttons">
+                    <a class="more" href="#">Купить</a>
+                </div>
+            </div>
+            <div class="promo-grid-square__item">
+                <img src="assets/newdis/img/certificate-3.jpg">
+                <h4 class="promo-grid-square__head"><a href="#">«Анти-стресс» курс из пяти посещений</a></h4>
+                <p class="promo-grid-square__description">2 Флоатинга с коучингом флоаттерапевта (по 60 мин) + 3 Флоатинга с массажем (по 60 мин)</p>
+                <p class="promo-grid-square__price">25000 руб.</p>
+                <div class="contentbox__buttons">
+                    <a class="more" href="#">Купить</a>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+
+    <section class="contentbox contentbox-not-margin">
+
+        <div class="promo-grid">
+            <div class="promo-grid__item">
+                <img src="assets/newdis/img/promo-10.png">
+                <p class="promo-grid__title">Для двоих</p>
+                <p>Программы на одновременное посещение двоих</p>
+            </div>
+        </div>
+
+        <div class="promo-grid-square">
+            <div class="promo-grid-square__item">
+                <img src="assets/newdis/img/certificate-1.jpg">
+                <h4 class="promo-grid-square__head"><a href="#">«Погружение» 60 минут</a></h4>
+                <p class="promo-grid-square__description">Флоатинг (60 мин.)</p>
+                <p class="promo-grid-square__price">4500 руб.</p>
+                <div class="contentbox__buttons">
+                    <a class="more" href="#">Купить</a>
+                </div>
+            </div>
+            <div class="promo-grid-square__item">
+                <img src="assets/newdis/img/certificate-2.jpg">
+                <h4 class="promo-grid-square__head"><a href="#">«Полное погружение» 120 минут</a></h4>
+                <p class="promo-grid-square__description">Флоатинг (60 мин) + Массаж (60 мин)</p>
+                <p class="promo-grid-square__price">9000 руб.</p>
+                <div class="contentbox__buttons">
+                    <a class="more" href="#">Купить</a>
+                </div>
+            </div>
+            <div class="promo-grid-square__item">
+                <img src="assets/newdis/img/certificate-3.jpg">
+                <h4 class="promo-grid-square__head"><a href="#">«Анти-стресс» курс из пяти посещений</a></h4>
+                <p class="promo-grid-square__description">2 Флоатинга с коучингом флоаттерапевта (по 60 мин) + 3 Флоатинга с массажем (по 60 мин)</p>
+                <p class="promo-grid-square__price">50000 руб.</p>
+                <div class="contentbox__buttons">
+                    <a class="more" href="#">Купить</a>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+
+    <section class="contentbox">
+
+        <div class="callme">
+            <h4>Перезвоните мне:</h4>
+            <form>
+                <input name="name" type="text" placeholder="Ваше имя"/>
+                <input name="phone" type="text" placeholder="Телефон"/>
+                <input name="submit" type="submit" value="Заказать звонок"/>
+            </form>
+        </div>
+
+    </section>
+
+    <section class="contentbox contentbox-not-margin">
+
+        <h4 class="contentbox__phones"><a href="tel:84991105210">+7 (499) 110-52-10</a></h4>
+
+    </section>
+
+
+</main>
+
+<?php include "footer.php" ?>
+
+</body>
+</html>
