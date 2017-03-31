f6841bd7309497d3f16c73bfa87a215a6dbb016d
Alexey Lavinsky <cap-Bernardito@yandex.ru>
2017-03-31 14:05:22 +0300

    main flex order fixed

--- index.php
+++ index.php
@@ -4,106 +4,98 @@
 <main id="content">
     <div class="cards-holder">
 
-        <div class="cards-holder-one">
-
-            <div class="card bg relax">
-                <div class="imgheader">
-                    <img src="assets/newdis/img/photo_relax.jpg" alt="">
-                    <h4>Релаксация<br>и&nbsp;восстановление</h4>
-                </div>
-                <div class="under-text">
-                    <p>Единственная в России<br>
-                        флоатинг-студия с бассейнами (не капсулами)<br>
-                        Уникальные <a href="#">программы глубинного восстановления</a>
-                        тела и разума.</p>
-
-                    <p class="center-but"><a class="more" href="[[~32]]">Подробнее</a></p>
-                </div>
+        <div class="card bg relax">
+            <div class="imgheader">
+                <img src="assets/newdis/img/photo_relax.jpg" alt="">
+                <h4>Релаксация<br>и&nbsp;восстановление</h4>
             </div>
+            <div class="under-text">
+                <p>Единственная в России<br>
+                    флоатинг-студия с бассейнами (не капсулами)<br>
+                    Уникальные <a href="#">программы глубинного восстановления</a>
+                    тела и разума.</p>
 
-            <div class="card nutrition">
-                <h2 class="title-nobg"><a href="#">Оздоровление питанием</a></h2>
-                <p>c помощью уникального сочетания опыта тибетской медицины,
-                    фитотерапии, ДНК-тестирования, детокса.</p>
-                <p class="left-but"><a class="more" href="[[~35]]">Подробнее</a></p>
+                <p class="center-but"><a class="more" href="[[~32]]">Подробнее</a></p>
             </div>
+        </div>
 
-            <div class="card bg beauty">
-                <div class="imgheader">
-                    <img src="assets/newdis/img/photo_beaty.jpg" alt="">
-                    <h4>Натуральная красота и&nbsp;омоложение</h4>
-                </div>
-                <div class="under-text">
-                    <p>Специалисты нашего центра разработали программу, которая
-                        стабилизирует внутренние процессы и помогает <a href="#">сохранить
-                            молодость и красоту</a> при современном ритме жизни.</p>
-                    <p class="center-but"><a class="more" href="[[~34]]">Подробнее</a></p>
-                </div>
+        <div class="card nutrition">
+            <h2 class="title-nobg"><a href="#">Оздоровление питанием</a></h2>
+            <p>c помощью уникального сочетания опыта тибетской медицины,
+                фитотерапии, ДНК-тестирования, детокса.</p>
+            <p class="left-but"><a class="more" href="[[~35]]">Подробнее</a></p>
+        </div>
+
+        <div class="card bg beauty">
+            <div class="imgheader">
+                <img src="assets/newdis/img/photo_beaty.jpg" alt="">
+                <h4>Натуральная красота и&nbsp;омоложение</h4>
             </div>
-
-            <div class="card recovery_text">
-                <h2 class="title-nobg"><a href="[[~37]]">Восстановление спортсменов...</a></h2>
-                <p>Избавиться от хронической усталости, снять тревожность и устранить боль
-                    поможет индивидуальный подход с
-                    помощью проверенных методов реального оздоровления организма:
-                    иглоукалывание, остеопатия,
-                    телесноориентированная терапия, лечебные массажи, флоатинг и т.д.</p>
-                <p class="left-but"><a class="more" href="#">Подробнее</a></p>
+            <div class="under-text">
+                <p>Специалисты нашего центра разработали программу, которая
+                    стабилизирует внутренние процессы и помогает <a href="#">сохранить
+                        молодость и красоту</a> при современном ритме жизни.</p>
+                <p class="center-but"><a class="more" href="[[~34]]">Подробнее</a></p>
             </div>
+        </div>
 
+        <div class="card recovery_text">
+            <h2 class="title-nobg"><a href="[[~37]]">Восстановление спортсменов...</a></h2>
+            <p>Избавиться от хронической усталости, снять тревожность и устранить боль
+                поможет индивидуальный подход с
+                помощью проверенных методов реального оздоровления организма:
+                иглоукалывание, остеопатия,
+                телесноориентированная терапия, лечебные массажи, флоатинг и т.д.</p>
+            <p class="left-but"><a class="more" href="#">Подробнее</a></p>
         </div>
 
         <img src="assets/newdis/img/scrolling.svg" alt="" class="scroll">
 
-        <div class="cards-holder-two">
+        <div class="card floating column-break">
+            <p><span class="title-nobg"><a href="#">Флоатинг</a></span> - самый
+                эффективный и быстрый метод глубочайшего расслабления и восстановления
+                за
+                60 минут, недостижимый другими способами. Результат сопоставим одной
+                ночи полноценного сна.</p>
+            <p class="left-but"><a class="more" href="[[~23]]">Подробнее о флоатинге</a></p>
+        </div>
 
-            <div class="card floating column-break">
-                <p><span class="title-nobg"><a href="#">Флоатинг</a></span> - самый
-                    эффективный и быстрый метод глубочайшего расслабления и восстановления
-                    за
-                    60 минут, недостижимый другими способами. Результат сопоставим одной
-                    ночи полноценного сна.</p>
-                <p class="left-but"><a class="more" href="[[~23]]">Подробнее о флоатинге</a></p>
+        <div class="card bg pregnancy">
+            <div class="imgheader">
+                <img src="assets/newdis/img/photo_pregnancy.jpg" alt="">
+                <h4>Здоровая беременность</h4>
             </div>
-
-            <div class="card bg pregnancy">
-                <div class="imgheader">
-                    <img src="assets/newdis/img/photo_pregnancy.jpg" alt="">
-                    <h4>Здоровая беременность</h4>
-                </div>
-                <div class="under-text">
-                    <p>Наши новейшие <a href="#">медицинские
-                            программы</a> ведения беременности<br>
-                        делают еще более легким<br>
-                        и радостным чудесное время<br>
-                        в жизни каждой женщины
-                    </p>
-                    <p class="center-but"><a class="more" href="[[~36]]">Подробнее</a></p>
-                </div>
-            </div>
-
-            <div class="card wellness">
-                <p><span class="title-nobg"><a href="#">Лайф и Wellness
-                            коучинг.</a></span>
-                    Текст
-                    ТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекст
+            <div class="under-text">
+                <p>Наши новейшие <a href="#">медицинские
+                        программы</a> ведения беременности<br>
+                    делают еще более легким<br>
+                    и радостным чудесное время<br>
+                    в жизни каждой женщины
                 </p>
-                <p class="left-but"><a class="more" href="#">Подробнее</a></p>
+                <p class="center-but"><a class="more" href="[[~36]]">Подробнее</a></p>
             </div>
+        </div>
 
-            <div class="card bg recovery">
-                <div class="imgheader">
-                    <img src="assets/newdis/img/photo_recovery.jpg" alt="">
-                    <h4>Восстановительная медицина</h4>
-                </div>
-                <div class="under-text">
-                    <p>Наши новейшие <a href="#">медицинские программы</a> ведения
-                        беременности делают еще более легким и радостным
-                        чудесное время в жизни каждой женщины.</p>
-                    <p class="center-but"><a class="more" href="[[~37]]">Подробнее</a></p>
-                </div>
+        <div class="card wellness">
+            <p><span class="title-nobg"><a href="#">Лайф и Wellness
+                        коучинг.</a></span>
+                Текст
+                ТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекстТекст
+            </p>
+            <p class="left-but"><a class="more" href="#">Подробнее</a></p>
+        </div>
+
+        <div class="card bg recovery">
+            <div class="imgheader">
+                <img src="assets/newdis/img/photo_recovery.jpg" alt="">
+                <h4>Восстановительная медицина</h4>
             </div>
-
+            <div class="under-text">
+                <p>Наши новейшие <a href="#">медицинские программы</a> ведения
+                    беременности делают еще более легким и радостным
+                    чудесное время в жизни каждой женщины.</p>
+                <p class="center-but"><a class="more" href="[[~37]]">Подробнее</a></p>
+            </div>
         </div>
 
     </div>
@@ -114,7 +106,7 @@
 
         <img class="photobox__img" src="assets/newdis/img/photo_about.jpg">
         <div class="photobox__wrap">
-            <div class="photobox__content">
+            <div class="photobox__content photobox__content-newformat">
                 <h4 class="photobox__head">О флоат-студии</h4>
                 <p>Мы верим в то, что из моментов счастья складывается наша жизнь. Наша
                     студия - это уникальное место гармонии в самом центре Москвы.</p>
